e71b8dff04feffdab899867a2989b7541fc33d81
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-07-10 00:01:39 +0530

    modified:   app/(root)/meeting/[id]/page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -9,9 +9,13 @@ import { useParams } from "next/navigation";
 import React, { useState } from "react";
 
 const page = ({ params }: { params: { id: string } }) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { id } = useParams();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { isLoaded, user } = useUser();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { call, isCallLoading } = useGetCallById(id);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
